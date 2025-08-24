d32084cceb9e19d70854bf8d7a6c3877f8b9e854
emheri <em.heri@gmail.com>
2025-08-24 23:33:49 +0700

    Ignore schema if exists

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_08_16_101032) do
-  create_schema "solid"
+  execute "CREATE SCHEMA IF NOT EXISTS solid"
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
