a9e6f02c63edd55d930bbc20af900a62e3b426c3
emheri <em.heri@gmail.com>
2025-08-24 23:48:08 +0700

    adjust cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # Change these
-set :repo_url,        'git@github.com:shirtpop/reordersweb.git'
-set :application,     'reordersweb'
-set :user,            'deploy'
-set :puma_threads,    [4, 16]
+set :repo_url,        "git@github.com:shirtpop/reordersweb.git"
+set :application,     "reordersweb"
+set :user,            "deploy"
+set :puma_threads,    [ 4, 16 ]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
@@ -34,15 +34,15 @@ set :linked_files, %w[config/google_client_secret.json config/google_token.yaml]
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
-  desc 'Initial Deploy'
+  desc "Initial Deploy"
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'pumactl:start'
-      invoke 'deploy'
+      before "deploy:restart", "pumactl:start"
+      invoke "deploy"
     end
   end
 
-  desc 'Create Directories for Puma Pids and Socket'
+  desc "Create Directories for Puma Pids and Socket"
   task :make_dirs do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
@@ -50,25 +50,25 @@ namespace :deploy do
     end
   end
 
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'pumactl:restart'
+      invoke "pumactl:restart"
     end
   end
 
-  desc 'Cleanup old assets'
+  desc "Cleanup old assets"
   task :clear_assets do
     on roles(:app) do
       within release_path do
-        execute :rm, '-rf', 'public/assets/*'
+        execute :rm, "-rf", "public/assets/*"
       end
     end
   end
 
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    'sitemap:refresh'
+  # after  :finishing,    'sitemap:refresh'
   after  :finishing,    :restart
   before :started, :make_dirs
   before :compile_assets, :clear_assets
