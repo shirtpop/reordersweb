1fd2286be0690604595d4d4247c1704eb98f39bc
emheri <em.heri@gmail.com>
2025-08-24 07:26:00 +0700

    add basic http auth on staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: 'shirtpop', password: 'awesome' if Rails.env.staging?
+
   include Pagy::Backend
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
