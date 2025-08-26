9ae3be4fc302455fb670122452626a4a7373cc48
emheri <em.heri@gmail.com>
2025-08-26 22:26:48 +0700

    change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,6 @@
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 4 }
-threads threads_count, threads_count
-workers ENV.fetch("WEB_CONCURRENCY") { 1 }
+threads ENV["RAILS_MIN_THREADS"], ENV["RAILS_MAX_THREADS"]
+workers ENV["WEB_CONCURRENCY"]
 preload_app!
 port ENV.fetch("PORT", 3000)
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
