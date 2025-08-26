6b1bd7e3f5ffb8459ae23a9b6a8beca54ed60de0
emheri <em.heri@gmail.com>
2025-08-26 21:55:52 +0700

    puma preload_app for solid queue

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+preload_app!
 #
 # Puma starts a configurable number of processes (workers) and each process
 # serves each request in a thread from an internal thread pool.
