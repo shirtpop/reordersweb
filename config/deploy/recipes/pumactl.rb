64e69570bd93b7c69ba02fb7d17d22b6fc9d680c
emheri <em.heri@gmail.com>
2025-09-05 09:55:14 +0700

    adjust warning linter

--- config/deploy/recipes/pumactl.rb
+++ config/deploy/recipes/pumactl.rb
@@ -3,31 +3,31 @@
 namespace :pumactl do
   task :start do
     on roles(:app) do
-      execute :sudo, :systemctl, 'start puma'
+      execute :sudo, :systemctl, "start puma"
     end
   end
 
   task :stop do
     on roles(:app) do
-      execute :sudo, :systemctl, 'stop puma'
+      execute :sudo, :systemctl, "stop puma"
     end
   end
 
   task :status do
     on roles(:app) do
-      execute :sudo, :systemctl, 'status puma'
+      execute :sudo, :systemctl, "status puma"
     end
   end
 
-  task 'phased-restart' do
+  task "phased-restart" do
     on roles(:app) do
-      execute :sudo, :systemctl, 'reload puma'
+      execute :sudo, :systemctl, "reload puma"
     end
   end
 
   task :restart do
     on roles(:app) do
-      execute :sudo, :systemctl, 'restart puma'
+      execute :sudo, :systemctl, "restart puma"
     end
   end
 end
