8550b5fffef896bb1c54798f393295b3f9c8466e
emheri <em.heri@gmail.com>
2025-09-05 09:11:42 +0700

    fix init flowbite and remove redundant import

--- app/javascript/client_application.js
+++ app/javascript/client_application.js
@@ -2,7 +2,11 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 import "@hotwired/turbo-rails"
-import "flowbite"
+import { initFlowbite } from "flowbite"
 
 eagerLoadControllersFrom("controllers/client", application)
 eagerLoadControllersFrom("controllers/shared", application)
+
+document.addEventListener("turbo:load", () => {
+  initFlowbite()
+})
