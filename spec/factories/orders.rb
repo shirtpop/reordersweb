0fa39e52e766f7ca3142716b636173408afafec8
emheri <em.heri@gmail.com>
2026-03-20 07:49:28 +0700

    adjust order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :order do
     client
-    catalog { create(:catalog, :with_products, client: client) }
     delivery_date { Time.current.to_date }
 
-    after(:build) do |order|
-      order.order_items = order.catalog.products.map do |product|
-        build(:order_item, order: order, product: product)
+    trait :with_catalog_line_items do
+      catalog { create(:catalog, :with_products, client: association(:client)) }
+      after(:build) do |order|
+        order.order_items = order.catalog.products.map do |product|
+          build(:order_item, order: order, product: product)
+        end
       end
     end
   end
