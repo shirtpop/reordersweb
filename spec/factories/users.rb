ae8406588a3dac578fcffa09379729f8b5700f63
M Heriyana <em.heri@gmail.com>
2026-08-30 10:03:29 +0700

    add first_name last_name user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     password { 'password123' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     role { 'admin' }
 
     trait :client do
